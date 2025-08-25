4f8a39892ae4ad4532ba5a171f0b635bfcb4f690
Sergii Kliuchnyk <redexp@ukr.net>
2025-08-25 12:27:42 +0300

    added city-regions on map

--- src/lib/map.ts
+++ src/lib/map.ts
@@ -22,63 +22,125 @@ export function createMap(container: string): Map {
 	});
 
 	map.on('load', function () {
-		map.addSource('pluscode-grid', {
-			type: 'geojson',
-			promoteId: 'plusCode',
-			data: {
-				type: 'FeatureCollection',
-				features: []
-			},
-		});
-
-		map.addLayer({
-			id: 'grid-fill',
-			type: 'fill',
-			source: 'pluscode-grid',
-			paint: {
-				'fill-color': [
-					'step', ['get', 'count'],
-					COLORS[0], 1, // < t1
-					COLORS[1], 2, // [t1, t2)
-					COLORS[2], 3, // [t2, t3)
-					COLORS[3] // >= t3
-				],
-				'fill-opacity': 0.75,
-			}
-		});
-
-		map.addLayer({
-			id: 'grid-outline',
-			type: 'line',
-			source: 'pluscode-grid',
-			paint: {
-				'line-color': '#8b0000',
-				'line-width': 0.6,
-				'line-opacity': 0.6
-			}
-		});
-
-		map.addLayer({
-			id: 'grid-labels',
-			type: 'symbol',
-			source: 'pluscode-grid',
-			layout: {
-				'text-field': ['to-string', ['get', 'count']],
-				'text-font': ['Open Sans Semibold','Arial Unicode MS Bold'],
-				'text-size': ['interpolate', ['linear'], ['zoom'], 10, 10, 14, 16],
-				'text-allow-overlap': true,
-				'text-ignore-placement': true
-			},
-			paint: {
-				'text-color': '#111',
-				'text-halo-color': '#fff',
-				'text-halo-width': 1.5,
-				'text-halo-blur': 0.5
-			}
-		});
+		addCityRegions(map);
+		addPlusCodeMapSource(map);
 	});
 
 	return map;
+}
+
+function addPlusCodeMapSource(map: Map) {
+	map.addSource('pluscode-grid', {
+		type: 'geojson',
+		promoteId: 'plusCode',
+		data: {
+			type: 'FeatureCollection',
+			features: []
+		},
+	});
+
+	map.addLayer({
+		id: 'grid-fill',
+		type: 'fill',
+		source: 'pluscode-grid',
+		paint: {
+			'fill-color': [
+				'step', ['get', 'count'],
+				COLORS[0], 1, // < t1
+				COLORS[1], 2, // [t1, t2)
+				COLORS[2], 3, // [t2, t3)
+				COLORS[3] // >= t3
+			],
+			'fill-opacity': 0.75,
+		}
+	});
+
+	map.addLayer({
+		id: 'grid-outline',
+		type: 'line',
+		source: 'pluscode-grid',
+		paint: {
+			'line-color': '#8b0000',
+			'line-width': 0.6,
+			'line-opacity': 0.6
+		}
+	});
+
+	map.addLayer({
+		id: 'grid-labels',
+		type: 'symbol',
+		source: 'pluscode-grid',
+		layout: {
+			'text-field': ['to-string', ['get', 'count']],
+			'text-font': ['Open Sans Semibold','Arial Unicode MS Bold'],
+			'text-size': ['interpolate', ['linear'], ['zoom'], 10, 10, 14, 16],
+			'text-allow-overlap': true,
+			'text-ignore-placement': true
+		},
+		paint: {
+			'text-color': '#111',
+			'text-halo-color': '#fff',
+			'text-halo-width': 1.5,
+			'text-halo-blur': 0.5
+		}
+	});
+}
+
+function addCityRegions(map: Map) {
+	map.addSource('city-regions', {
+		type: 'geojson',
+		data: '/geo/city-regions.json'
+	});
+
+	map.addLayer({
+		id: 'city-regions-fill',
+		type: 'fill',
+		source: 'city-regions',
+		paint: {
+			'fill-color': '#1d5be1',
+			'fill-opacity': 0.3
+		},
+		filter: ['==', ['geometry-type'], 'Polygon'],
+	});
+
+	map.addLayer({
+		id: 'city-regions-line',
+		type: 'line',
+		source: 'city-regions',
+		paint: {
+			'line-width': 1
+		},
+		filter: ['==', ['geometry-type'], 'Polygon']
+	});
+
+	map.addLayer({
+		id: 'city-regions-label',
+		type: 'symbol',
+		source: 'city-regions',
+		layout: {
+			'text-field': ['get', 'name'],
+			'text-size': 12,
+			'text-font': ['Open Sans Bold', 'Arial Unicode MS Bold'],
+			'text-allow-overlap': false,
+			'text-ignore-placement': false
+		},
+		paint: {
+			'text-color': '#111',
+			'text-halo-color': '#fff',
+			'text-halo-width': 1.2
+		},
+		filter: ['==', ['geometry-type'], 'Polygon']
+	});
+
+	toggleCityRegions(map, false);
+}
+
+export function toggleCityRegions(map: Map, visible: boolean) {
+	map.getStyle().layers
+	.filter(l => l.source === 'city-regions')
+	.forEach(l => {
+		map.setLayoutProperty(l.id, 'visibility', visible ? 'visible' : 'none');
+	});
 }
 
 export function updateMapData(map: Map, data: GeoData) {
