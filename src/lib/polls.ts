2ec24d1678be91ea6e2ad215ebaf97b983d139ee
Sergii Kliuchnyk <redexp@ukr.net>
2025-10-22 17:20:47 +0300

    split polls on main page

--- src/lib/polls.ts
+++ src/lib/polls.ts
@@ -12,3 +12,20 @@ export function getPolls(folder?: string): Poll[] {
 	return polls.filter(p => p.id.startsWith(folder + '/'));
 }
 
+export function getActivePastPolls(): [Poll[], Poll[]] {
+	const now = new Date();
+
+	const active: Poll[] = [];
+	const past: Poll[] = [];
+
+	for (const poll of getPolls()) {
+		if (!poll.data.expire || now < poll.data.expire) {
+			active.push(poll);
+		}
+		else {
+			past.push(poll);
+		}
+	}
+
+	return [active, past];
+}
